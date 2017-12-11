f7f93ef12ada5c7eaf40574400d2075175ba8941
marcinj95 <mojgt540@gmail.com>
2017-12-11 08:05:06 +0100

    Small steps forward...

--- app/src/main/java/com/example/marci/autoradar/MainActivity.java
+++ app/src/main/java/com/example/marci/autoradar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.marci.autoradar;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.List;
+
+import adapters.AutoListAdapter;
+import entities.Auto;
+import models.AutoRestClient;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +48,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        new MainActivity.HttpRequestAsk().execute();
+
+
     }
 
     @Override
@@ -97,5 +110,24 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+    private class HttpRequestAsk extends AsyncTask<Void, Void, List<Auto>> {
+
+        @Override
+        protected List<Auto> doInBackground(Void... voids) {
+            AutoRestClient autoRestClient = new AutoRestClient();
+            return autoRestClient.finAll();
+
+        }
+
+        @Override
+        protected void onPostExecute(List<Auto> autos) {
+            ListView listViewAuto = findViewById(R.id.listViewAutoContentMain);
+            listViewAuto.setAdapter(new AutoListAdapter(MainActivity.this, autos));
+
+        }
     }
 }
